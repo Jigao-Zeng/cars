55a85eb492679248995cfe887fae3be5f8b2f6ec
Jigao <jigao@nuimarkets.com>
2024-09-19 11:34:27 +1200

    error handling details and remove fonts folder

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -4,7 +4,7 @@ export async function fetchCarWithId(id: string) {
   try {
     const response = await fetch(`https://freetestapi.com/api/v1/cars/${id}`);
     if (!response.ok) {
-      throw new Error("Failed to fetch car with id.");
+      throw new Error("Failed to fetch car with id, ressponse not ok");
     }
     const car: Car = await response.json();
     return car;
@@ -20,7 +20,7 @@ export async function fetchSearchedCars(query: string) {
       `https://freetestapi.com/api/v1/cars?search=${query}`
     );
     if (!response.ok) {
-      throw new Error("Failed to fetch car with id.");
+      throw new Error("Failed to fetch car lists, response not ok");
     }
     const cars: Car[] = await response.json();
     return cars;
